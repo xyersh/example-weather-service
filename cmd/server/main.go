3d6d3bf6a6a5705eac7751f3199bceeef8e242d3
Alex Yersh <xyersh@gmail.com>
2025-11-06 21:05:12 +0300

    add crone

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-co-op/gocron/v2"
+)
+
+const (
+	httpPort = ":3000"
 )
 
 func main() {
+	wg := sync.WaitGroup{}
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -17,8 +27,74 @@ func main() {
 			log.Println(err)
 		}
 	})
-	err := http.ListenAndServe(":3000", r)
+
+	shed, err := initCron()
 	if err != nil {
 		panic(err)
 	}
+
+	_, err = initJobs(shed)
+	if err != nil {
+		panic(err)
+	}
+
+	wg.Go(func() {
+		err := http.ListenAndServe(httpPort, r)
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	wg.Go(func() {
+		fmt.Println("sheduler starts")
+		shed.Start()
+	})
+
+	wg.Wait()
+}
+
+func initCron() (gocron.Scheduler, error) {
+	fmt.Println("initCron - START")
+	s, err := gocron.NewScheduler()
+	if err != nil {
+		return nil, err
+	}
+	return s, nil
+}
+
+func initJobs(shed gocron.Scheduler) ([]gocron.Job, error) {
+	fmt.Println("initJobs - START")
+	// add a job to the scheduler
+
+	j, err := shed.NewJob(
+		gocron.DurationJob(
+			10*time.Second,
+		),
+		gocron.NewTask(
+			func() {
+				fmt.Printf("%v hello\n", time.Now())
+			},
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return []gocron.Job{j}, nil
+}
+
+func runCron(shed gocron.Scheduler) error {
+	fmt.Println("runCron - START")
+	// start the scheduler
+	shed.Start()
+
+	// block until you are ready to shut down
+	<-time.After(time.Minute)
+
+	// when you're done, shut it down
+	err := shed.Shutdown()
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/xyersh/example-weather-service
 
 go 1.25.0
 
-require github.com/go-chi/chi/v5 v5.2.3 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.3 // indirect
+	github.com/go-co-op/gocron/v2 v2.18.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jonboulle/clockwork v0.5.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+)
