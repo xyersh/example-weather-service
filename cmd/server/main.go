5f1153fb2e094be5c70bacc2dd7cb713b279460e
Alex Yersh <xyersh@gmail.com>
2025-11-06 12:02:30 +0300

    initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("welcome"))
+		if err != nil {
+			log.Println(err)
+		}
+	})
+	err := http.ListenAndServe(":3000", r)
+	if err != nil {
+		panic(err)
+	}
+}
